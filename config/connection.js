8474c21333f24d635badc6bb0613f5378cb95a10
Katie Lejman <misskatiemary@gmail.com>
2017-11-16 21:46:49 -0500

    connection update

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,27 @@
 // Set up MySQL connection.
     const mysql = require('mysql');
-    let connection;
 
-// heroku else mysql
-    if (process.env.JAWSDB_URL) {
-        connection = mysql.createConnection(process.env.JAWSDB_URL);
-    }
-    else {
-        connection = mysql.createConnection({
+    let connection = mysql.createConnection({
             host: "localhost",
             user: "root",
             password: "",
             database: "burgers_db"
         });
-    }
 
+// heroku else mysql
+//     if (process.env.JAWSDB_URL) {
+//         connection = mysql.createConnection(process.env.JAWSDB_URL);
+//     }
+//     else {
+//         connection = mysql.createConnection({
+//             host: "localhost",
+//             user: "root",
+//             password: "",
+//             database: "burgers_db"
+//         });
+//     }
 
-
-/*
-NOTE: module.exports EXPORTS OBJECTS LISTED TO THE RIGHT OF THE EQUAL SIGN THEREFORE
+/*NOTE: module.exports EXPORTS OBJECTS LISTED TO THE RIGHT OF THE EQUAL SIGN THEREFORE
 literally what we are doing is exporting the connection object
 */
 
