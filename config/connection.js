ff7d0e40d593df22bf60c6d9bd8ab4478c4c16ad
Katie Lejman <misskatiemary@gmail.com>
2017-11-17 23:42:47 -0500

    added jawsDB into config file

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,20 @@
 // Set up MySQL connection.
     const mysql = require('mysql');
 
-    let connection = mysql.createConnection({
+    let connection;
+
+// heroku else mysql
+    if (process.env.JAWSDB_URL) {
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    }
+    else {
+        connection = mysql.createConnection({
             host: "localhost",
             user: "root",
             password: "",
             database: "burgers_db"
         });
-
-// heroku else mysql
-//     if (process.env.JAWSDB_URL) {
-//         connection = mysql.createConnection(process.env.JAWSDB_URL);
-//     }
-//     else {
-//         connection = mysql.createConnection({
-//             host: "localhost",
-//             user: "root",
-//             password: "",
-//             database: "burgers_db"
-//         });
-//     }
+    }
 
 /*NOTE: module.exports EXPORTS OBJECTS LISTED TO THE RIGHT OF THE EQUAL SIGN THEREFORE
 literally what we are doing is exporting the connection object
