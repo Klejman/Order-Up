25212a5f2d65c94ec2f474d274a579d83591f8e6
Katie Lejman <misskatiemary@gmail.com>
2017-11-21 18:52:10 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,27 @@
 // Set up MySQL connection.
     const mysql = require('mysql');
 
-    const connection = mysql.createConnection(process.env.JAWSDB_URL || {
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: "",
-        database: 'burgers_db'
-    });
+    if(process.env.JAWSDB_URL) {
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: "",
+            database: 'burgers_db'
+        })
+    };
 
-
-
-    connection.connect(function(err) {
-        if (err) {
-            console.error(`error connection: ${err.stack}`);
-            return;
-        }
-        console.log(`connected as id ${connection.threadId}`);
-        });
-
+    connection.connect();
+    
+    // connection.connect(function(err) {
+    //     if (err) {
+    //         console.error(`error connection: ${err.stack}`);
+    //         return;
+    //     }
+    //     console.log(`connected as id ${connection.threadId}`);
+    //     });
+    //
 
 
 /*NOTE: module.exports EXPORTS OBJECTS LISTED TO THE RIGHT OF THE EQUAL SIGN THEREFORE
--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,16 @@ USE burgers_db;
 
 
 CREATE TABLE burgers (
-id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+id INT AUTO_INCREMENT NOT NULL,
+createdAt: TIMESTAMP NOT NULL,
 burger_name VARCHAR(50) NOT NULL,
 devoured BOOL DEFAULT FALSE,
-datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
 );
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('portobello mushroom burger ', TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('lamb burger with goat cheese & relish', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Ron Swanson turkey leg burger',  TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('elk burger',  FALSE);
+
